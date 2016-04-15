50d629ee3202b816792fa0069c521ef9ad6efc8e
cutsea110 <cutsea110@gmail.com>
2016-04-16 05:10:10 +0900

    update

--- src/Text/CSharp.hs
+++ src/Text/CSharp.hs
@@ -20,6 +20,8 @@ data Content = Raw String
              | CForall String [E]
              | CMaybe String [E]
              | CNothing
+             | CIf [E]
+             | CElse
                deriving Show
 
 data E = S String
@@ -34,10 +36,16 @@ eol =     try (string "\n\r")
       <|> string "\r"
       <?> fail "end of line"
 
+spaceTabs :: Parser String
+spaceTabs = many (oneOf " \t")
+
 parser :: Parser [Content]
 parser = many (try embed <|>
                try cforall <|>
                try cmaybe <|>
+               try cnothing <|>
+               try cif <|>
+               try celse <|>
                raw)
 
 embed :: Parser Content
@@ -47,7 +55,7 @@ expr :: Parser [E]
 expr = many1 term
     where
       term :: Parser E
-      term = spaces *> (S <$> str <|> I <$> integer <|> V <$> var)
+      term = spaceTabs *> (S <$> str <|> I <$> integer <|> V <$> var) <* spaceTabs
 
 integer :: Parser Integer
 integer = read <$> many1 digit
@@ -59,30 +67,36 @@ str = char '"' *> many quotedChar <* char '"'
       quotedChar = noneOf "\\\"" <|> try (string "\\\"" >> return '"')
 
 var :: Parser String
-var = many1 (noneOf " \t}")
+var = many1 (noneOf " \t\n\r{}#")
 
 raw :: Parser Content
-raw = Raw <$> many1 (noneOf "#")
+raw = Raw <$> many1 (noneOf "#\n\r")
 
 cforall :: Parser Content
-cforall = CForall <$> bindVal <*> expr
+cforall = CForall <$> bindVal <*> expr <* eol
     where
-      bindVal = string "#forall" *> spaces *>
+      bindVal = string "#forall" *> spaceTabs *>
                 binding
-                <* spaces <* string "<-" <* spaces
+                <* spaceTabs <* string "<-" <* spaceTabs
 
 cmaybe :: Parser Content
-cmaybe = CMaybe <$> bindVal <*> expr
+cmaybe = CMaybe <$> bindVal <*> expr <* eol
     where
-      bindVal = string "#maybe" *> spaces *>
+      bindVal = string "#maybe" *> spaceTabs *>
                 binding
-                <* spaces <* string "<-" <* spaces
+                <* spaceTabs <* string "<-" <* spaceTabs
 
 cnothing :: Parser Content
-cnothing = string "#nothing" *> many (noneOf "\r\n") *> eol >> return CNothing
+cnothing = string "#nothing" *> spaceTabs *> eol *> pure CNothing
+
+cif :: Parser Content
+cif = CIf <$> (string "#if" *> spaceTabs *> expr <* spaceTabs <* eol)
+
+celse :: Parser Content
+celse = string "#else" *> spaceTabs *> eol *> pure CElse
 
 -- TODO: support pattern match
 binding :: Parser String
 binding = many1 (letter <|> digit <|> char '_')
 
--- | >>> parse parser "" "hello world#{foo var 12 \"ok\"} cutsea #{var}"
+-- | >>> parse parser "" "hello world#{foo var 12 \"ok\"} cutsea #{var} #forall x <- f 1 2 3  \t \nHello #if b 1 2 \n Hi,,  #else \n foo #maybe x <- mx 1 2 \n Bye#nothing  \n"
